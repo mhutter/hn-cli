c80eeca77720190780d907d6e2adf9113cc987c3
Manuel Hutter <manuel.hutter@vshn.ch>
2016-09-12 13:58:54 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,25 @@
+# hn-cli
+
+Minimal Hacker News CLI.
+
+
+## Installation
+
+    go get -u github.com/mhutter/hn-cli/hn
+
+
+## Usage
+
+    hn [number of stories]
+
+**number of stories** defaults to 10
+
+
+## Examples
+
+![Screenshot](screenshot.png)
+
+
+## TODO
+
+- [ ] local caching
---
+++ hn/cli.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/peterhellberg/hn"
+)
+
+var numStories = 10
+var client *hn.Client
+var cache []*hn.Item
+
+func init() {
+	var err error
+	if len(os.Args) > 1 {
+		numStories, err = strconv.Atoi(os.Args[1])
+		e(err)
+	}
+}
+
+func main() {
+	client = hn.DefaultClient
+
+	ids, err := client.TopStories()
+	e(err)
+
+	// fetch data
+	pending := numStories
+	c := make(chan bool)
+	cache = make([]*hn.Item, numStories, numStories)
+
+	for i, id := range ids[:numStories] {
+		go fetch(i, id, cache, c)
+	}
+
+	for pending > 0 {
+		<-c
+		pending--
+	}
+
+	for i, item := range cache {
+		fmt.Printf("%2d. \033[1m%s\033[0m\n    \033[2m%s\033[0m\n",
+			i, item.Title, item.URL)
+	}
+}
+
+func fetch(i, id int, cache []*hn.Item, c chan bool) {
+	var err error
+	cache[i], err = client.Item(id)
+	e(err)
+	c <- true
+}
+
+func e(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
